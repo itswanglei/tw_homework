4cd93ce63302868358e78fa3419611ae9eeff6d4
WangLei <wanglei0717@outlook.com>
2018-11-23 14:16:42 +0800

    opt: remove redundant function and variables

--- arrayInJS/js/arrayJoin.js
+++ arrayInJS/js/arrayJoin.js
@@ -1,12 +1,8 @@
-function joinArrayWithSymbol(array, symbol) {
-  return array.join(symbol);
-}
-
 const colors = ["Red", "Green", "White", "Black"];
 
-const colorsJoinWithBlank = joinArrayWithSymbol(colors, " ");
-const colorsJoinWithPlus = joinArrayWithSymbol(colors, "+");
-const colorsJoinWithComma = joinArrayWithSymbol(colors, ",");
+const colorsJoinWithBlank = colors.join(" ");
+const colorsJoinWithPlus = colors.join("+");
+const colorsJoinWithComma = colors.join(",");
 
 document.write("case 1 output: " + colorsJoinWithBlank + "</br>");
 document.write("case 2 output: " + colorsJoinWithPlus + "</br>");
