a19c7f4f7ba0b395d1da36287ad0d5f98adb7224
WangLei <wanglei0717@outlook.com>
2018-11-27 09:37:23 +0800

    Correct a mistake in 'reverse_string.js'

--- js_function/js/reverse_string.js
+++ js_function/js/reverse_string.js
@@ -1,7 +1,7 @@
 function reverseString(string) {
   let stringContentArray = new Array(string.length);
   for (let i = 0; i < string.length; i++) {
-    stringContentArray[string.length - i] = string[i];
+    stringContentArray[string.length - i -1] = string[i];
   }
   return stringContentArray.join("");
 }
