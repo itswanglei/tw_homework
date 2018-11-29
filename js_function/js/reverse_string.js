90ceeb28b12c0e4fdfa676b37bfb5c994d968b80
WangLei <wanglei0717@outlook.com>
2018-11-29 20:27:37 +0800

    Add a new method to reverse a string

--- js_function/js/reverse_string.js
+++ js_function/js/reverse_string.js
@@ -1,9 +1,15 @@
+// Method 1
 function reverseString(string) {
   let stringContentArray = new Array(string.length);
   for (let i = 0; i < string.length; i++) {
     stringContentArray[string.length - i -1] = string[i];
   }
   return stringContentArray.join("");
+}
+
+// Method 2: reverse string by convert string to array
+function reverseStringByArrayReverse(string) {
+  return string.split("").reverse().join("");
 }
 
 const inputString = "hello";
