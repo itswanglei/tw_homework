7ab584adc296f3ac31d9a8b67710e395338c8689
WangLei <wanglei0717@outlook.com>
2018-11-29 20:28:24 +0800

    Add a new method to judge palindrome

--- js_function/js/judge_palindrome.js
+++ js_function/js/judge_palindrome.js
@@ -1,3 +1,4 @@
+// Method 1
 function palindrome(string) {
   for (let i = 0; i < string.length; i++) {
     if (string[i] != string[string.length - i - 1]) {
@@ -5,6 +6,11 @@ function palindrome(string) {
     }
   }
   return true;
+}
+
+// Method 2
+function isPalindrome(string) {
+  return (string.split("").reverse().join("") === string);
 }
 
 document.write("Is 'hello' palindrome? " + palindrome("hello") + "</br>");
