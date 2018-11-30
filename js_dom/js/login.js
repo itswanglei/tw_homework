eac111e79c51470aed52d462172cfb35101e3dac
WangLei <wanglei0717@outlook.com>
2018-11-30 12:36:18 +0800

    Refactor code

--- js_dom/js/login.js
+++ js_dom/js/login.js
@@ -1,10 +1,30 @@
-function verifyUsernameAndPassword() {
+function isLoginSuccess(username, password) {
+  return username === "123" && password === "123";
+}
+
+function showLoginResult() {
   let username = document.getElementById("username").value;
   let password = document.getElementById("password").value;
-  if (username != "123" || password != "123") {
-    alert("用户名或密码错误！");
-    document.getElementById("password").value = "";
+  if (isLoginSuccess(username, password)) {
+    removeTips();
+    creatTips("登录成功！");
   } else {
-    alert("登录成功！")
+    removeTips();
+    creatTips("用户名或密码错误！");
+  }
+}
+
+function creatTips(tipString) {
+  let tipPara = document.createElement("p");
+  tipPara.setAttribute("id", "tipPara");
+  let tip = document.createTextNode(tipString);
+  tipPara.appendChild(tip);
+  document.body.appendChild(tipPara);
+}
+
+function removeTips() {
+  let tipPara = document.getElementById("tipPara");
+  if (tipPara) {
+    tipPara.parentNode.removeChild(tipPara);
   }
 }
