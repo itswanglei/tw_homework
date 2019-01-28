8d515cf6f02c5ef7512fe9c3ef0e6591c61706d1
WangLei <wanglei0717@outlook.com>
2019-01-28 23:29:57 +0800

    Practice to use interface

---
+++ java_oop/interface/ChooseTransportation/src/Transportation.java
@@ -0,0 +1,9 @@
+public interface Transportation {
+  boolean takeSelfDriving(double money);
+
+  boolean takeBus(double money);
+
+  boolean takeTrain(double money);
+
+  boolean takePlane(double money);
+}
