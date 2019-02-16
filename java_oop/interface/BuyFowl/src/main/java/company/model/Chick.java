fbfe3d61c2e597ddcc2be0f75ca49f3315c639a6
WangLei <wanglei0717@outlook.com>
2019-02-16 11:05:05 +0800

    Refactor the chick class

--- java_oop/interface/BuyFowl/src/main/java/company/model/Chick.java
+++ java_oop/interface/BuyFowl/src/main/java/company/model/Chick.java
@@ -2,11 +2,11 @@ package main.java.company.model;
 
 public class Chick extends Fowl {
     public Chick() {
-        super(0.33);
+        super(1 / 3);
     }
 
     @Override
     public int setMaxQuantity(double money) {
-        return (int) money * 3;
+        return (int) Math.floor(money / this.getPrice());
     }
 }
