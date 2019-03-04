a217a26315527b0809bdac08c42e4b1d3fd627d4
WangLei <wanglei0717@outlook.com>
2019-03-04 12:03:49 +0800

    Add code to close file

--- java_file_operation/CopyDirectory/src/main/java/company/Main.java
+++ java_file_operation/CopyDirectory/src/main/java/company/Main.java
@@ -1,5 +1,7 @@
 package main.java.company;
 
+import java.io.File;
+
 public class Main {
 
     public static final String ORIGIN_DIRECTORY = "./docs/users";
@@ -9,5 +11,15 @@ public class Main {
         Copy copy = new Copy(ORIGIN_DIRECTORY, TARGET_PATH);
         copy.copyDirectory();
         copy.copyFiles();
+
+//        File newDir = new File("./docs/copy_of_users");
+//        File[] files = newDir.listFiles();
+//        if (null == files) {
+//            newDir.delete();
+//        }
+//        for (File file : files) {
+//            file.delete();
+//        }
+//        newDir.delete();
     }
 }
